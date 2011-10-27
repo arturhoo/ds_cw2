d15d933ed1ea4c1d3de249d55cf086040990325a
Artur Rodrigues <arturhoo@gmail.com>
2011-10-27 19:54:38 +0100

    first attempt question 1

--- ex2/CityImpl.java
+++ ex2/CityImpl.java
@@ -24,43 +24,44 @@ public class CityImpl extends UnicastRemoteObject implements City {
     // return the City's country name
 
     public String getCountry() throws RemoteException{
-
+      return country;
    }
 
 
     // return the City's min temperature
 
    public int getMinTemperature() throws RemoteException {
-
+      return minTemperature;
    }
 
       // return the City's max temperature
 
    public int getMaxTemperature() throws RemoteException {
-
+      return maxTemperature;
    }
 
       // return the City's average temperature
 
    public double getAvgTemperature() throws RemoteException {
-
+      double avgTemperature = (maxTemperature/minTemperature)/2;
+      return avgTemperature;
    }
 
     // set the City's min temperature
 
-   public void setCountry(String c) throws RemoteException {
-
+   public void setCountry(String country) throws RemoteException {
+      this.country = country;
    }
 
     // set the City's min temperature
 
    public void setMinTemperature(int min) throws RemoteException {
-
+    this.minTemperature = min;
    }
 
       // return the City's max temperature
 
    public void setMaxTemperature(int max) throws RemoteException {
-
+    this.maxTemperature = max;
    }
 }
